848935a062e3b5a23640b0f45b3ff2f8952381b6
jewoodev <jewoos15@naver.com>
2025-12-23 16:03:17 +0900

    🔨 refactor: 프론트 컨트롤러로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -14,20 +14,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.springframework.http.HttpMethod.GET;
+
 @SpringBootApplication
 public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-                    String name = req.getParameter("name");
-                    res.setStatus(HttpStatus.OK.value());
-                    res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    res.getWriter().println("Hello, " + name);
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(GET.name())) {
+                        String name = req.getParameter("name");
+                        res.setStatus(HttpStatus.OK.value());
+                        res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        res.getWriter().println("Hello, " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+
+                    } else {
+                        res.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
