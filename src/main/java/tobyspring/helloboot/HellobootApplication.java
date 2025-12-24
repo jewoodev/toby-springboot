d8c347366d4d3fe1cd95eb4a665fbba553f1bf45
jewoodev <jewoos15@naver.com>
2025-12-24 12:15:13 +0900

    🔨 refactor: 스프링 컨테이너 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,23 +20,23 @@ import static org.springframework.http.HttpMethod.GET;
 @SpringBootApplication
 public class HellobootApplication {
     public static void main(String[] args) {
+        var applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            var helloController = new HelloController();
-
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(GET.name())) {
                         String name = req.getParameter("name");
+
+                        var helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        res.setStatus(HttpStatus.OK.value());
-                        res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        res.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         res.getWriter().println(ret);
-                    }
-                    else if (req.getRequestURI().equals("/user")) {
-
                     }
                     else {
                         res.setStatus(HttpStatus.NOT_FOUND.value());
