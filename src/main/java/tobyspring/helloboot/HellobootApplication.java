a6db176a7bc09136bc633ffda080d4c60603ae00
jewoodev <jewoos15@naver.com>
2025-12-24 13:28:29 +0900

    🔨 refactor: DispatchServlet으로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,17 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static org.springframework.http.HttpMethod.GET;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class HellobootApplication {
@@ -26,23 +17,8 @@ public class HellobootApplication {
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(GET.name())) {
-                        String name = req.getParameter("name");
-
-                        var helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        res.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        res.getWriter().println(ret);
-                    }
-                    else {
-                        res.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+                    .addMapping("/*");
         });
         webServer.start();
     }
