8da5fbaefc12b689f4a17656503bbf9251e8578f
jewoodev <jewoos15@naver.com>
2025-12-29 16:33:20 +0900

    🔨 refactor: 스타터와 Jetty 서버 구성 추가

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
     @Bean
-    public ServletWebServerFactory tomcatWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory jettyWebServerFactory() {
+        return new JettyServletWebServerFactory();
     }
 }
