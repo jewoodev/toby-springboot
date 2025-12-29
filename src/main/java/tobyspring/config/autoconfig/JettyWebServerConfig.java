6763470eda0effe38fecdc5282c71cdaa60ca466
jewoodev <jewoos15@naver.com>
2025-12-29 16:53:54 +0900

    🔨 refactor: `@Conditional`과 `Condition`

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -3,12 +3,26 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
 
+import static tobyspring.config.autoconfig.JettyWebServerConfig.*;
+
 @MyAutoConfiguration
+@Conditional(JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean
     public ServletWebServerFactory jettyWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
