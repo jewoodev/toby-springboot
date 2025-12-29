22c8bf09454d8b91b72d4b82cf5485046bc9a4e9
jewoodev <jewoos15@naver.com>
2025-12-29 14:15:58 +0900

    🔨 refactor: 합성 애노테이션의 적용

--- src/main/java/tobyspring/helloboot/HelloConfig.java
+++ src/main/java/tobyspring/helloboot/HelloConfig.java
@@ -1,10 +1,23 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 public class HelloConfig {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     @Bean
     public HelloService helloService() {
         return new HelloDecorator(new SimpleHelloService());
